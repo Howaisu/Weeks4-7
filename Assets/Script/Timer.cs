85ebbf1cdff985d043b5f7a88a949d10dc0aca5a
XiangqiMeng <m332296992@gmail.com>
2025-02-06 09:55:11 -0500

    Finished Timer

--- Assets/Script/Timer.cs
+++ Assets/Script/Timer.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
+    public GameObject txt;
+    TextMeshProUGUI text;
+     Slider slider;
+    float t;
     // Start is called before the first frame update
     void Start()
     {
-        
+        slider = GetComponent<Slider>();
+        text = txt.GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        t += Time.deltaTime;
+        slider.value = t % slider.maxValue;
+        text.text = ((int)(slider.value*100)).ToString();
+        //if (slider.value > 1)
+        //{
+        //    slider.value = 0;
+        //}
     }
 }
