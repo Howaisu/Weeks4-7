841cd63829c5e00d6ce9fe3f880131589be2f87e
XiangqiMeng <m332296992@gmail.com>
2025-02-18 15:52:15 -0500

    Fish head rotation experiment, compelet

--- Assets/Assignment2/script/GameController.cs
+++ Assets/Assignment2/script/GameController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public GameObject playerFish;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,23 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        spinMaker();
     }
+
+    // Method to make the circle face the mouse position
+    public void spinMaker()
+    {
+        // Get the mouse position in world space
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // Calculate the direction vector from the circle to the mouse
+        Vector3 direction = mousePos - transform.position;
+
+        // Find the angle (in radians) and convert it to degrees
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        // Set the rotation of the circle (only on the z-axis)
+        playerFish.transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
 }
