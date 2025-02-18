de873afbf7edf569d735d92ab0aa69e2c982249a
XiangqiMeng <m332296992@gmail.com>
2025-02-18 16:01:42 -0500

    movement experiment, complete

--- Assets/Assignment2/script/GameController.cs
+++ Assets/Assignment2/script/GameController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     public GameObject playerFish;
+    public Vector3 mousePos;
+    public float playerSpeed = 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,24 +15,42 @@ public class GameController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    { 
+        // Get the mouse position in world space (put it here because it may re-use multiple times
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //head rotation
         spinMaker();
+        //moving
+        Move();
     }
 
     // Method to make the circle face the mouse position
     public void spinMaker()
     {
-        // Get the mouse position in world space
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+       
+        
 
         // Calculate the direction vector from the circle to the mouse
         Vector3 direction = mousePos - transform.position;
 
-        // Find the angle (in radians) and convert it to degrees
+        // Find the angle (in radians) and convert it to degrees(circle)
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-        // Set the rotation of the circle (only on the z-axis)
+        // Set the rotation angle to the circle or use transform.eulerAngle=rotation
         playerFish.transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
+    void Move()
+    {
+        // transfer mouse position to world space
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0; //lock the z
+
+        // get the direction from fish to mouse
+        Vector3 direction = (mousePos - playerFish.transform.position).normalized;
+
+        // Move playerFish in the direction of the mouse
+        playerFish.transform.position += direction * playerSpeed * Time.deltaTime;
     }
 
 }
